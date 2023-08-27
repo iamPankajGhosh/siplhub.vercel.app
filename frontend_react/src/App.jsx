ce9b153c21f6d068c542e3b31d63d75386efdc53
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-08-27 11:39:58 +0530

    Integrate : react router

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,7 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, About, Services, Contact, Error } from "./pages";
+
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          {["/", "/home"].map((path) => (
+            <Route exact path={path} element={<Home />} />
+          ))}
+
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/services" element={<Services />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
