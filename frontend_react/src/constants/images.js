f6047750445125180b10372c519088b29b89962e
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-08-27 11:38:49 +0530

    Create : home page

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,26 @@
+/**
+ * hero images
+ */
+
+import heroBanner from "../assets/img/hero-banner.png"
+
+/**
+ * service images
+ */
+
+import serviceIcon1 from "../assets/img/service-icon-1.png"
+import serviceIcon2 from "../assets/img/service-icon-2.png"
+import serviceIcon3 from "../assets/img/service-icon-3.png"
+import serviceIcon4 from "../assets/img/service-icon-4.png"
+
+/**
+ * export all images
+ */
+
+export default {
+    heroBanner,
+    serviceIcon1,
+    serviceIcon2,
+    serviceIcon3,
+    serviceIcon4
+}
