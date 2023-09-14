a1544f6a8d50eeddb7fcd27835a11b074de8f1cd
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-09-14 17:13:32 +0530

    Refactor : remove hero

--- frontend_react/src/pages/Services.jsx
+++ frontend_react/src/pages/Services.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Preloader, Navbar, Footer } from "../components";
 import { Hero, Service } from "../containers";
+import images from "../constants/images";
 
 export default function Services() {
   return (
@@ -13,8 +14,8 @@ export default function Services() {
           <Hero
             title="Elevate Your Business with Our Exceptional Services"
             text="In today's dynamic business landscape, effective management is paramount to achieving success and staying competitive. At Sikharthy Infotect, we are committed to helping you take charge of your business and propel it to new heights through our comprehensive suite of services."
+            heroBanner={images.heroBanner}
           />
-          <Hero />
           <Service />
         </article>
       </main>
