8a36db00396e7eeeca7742493c7b4c5cf3b76719
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-02-01 23:47:20 +0530

    button component done

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Preloader } from "./Preloader/Preloader";
+export { default as Button } from "./Button/Button";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Footer } from "./Footer/Footer";
 export { default as VideoPlayer } from "./VideoPlayer/VideoPlayer";
