8903aa9d3f96e2dba282bd31ad46c15e69249809
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-02-09 17:44:56 +0530

    initialize project with new folder structure

--- frontend_react/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Preloader } from "./Preloader/Preloader";
-export { default as Logo } from "./Logo/Logo";
-export { default as Button } from "./Button/Button";
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Footer } from "./Footer/Footer";
-export { default as VideoPlayer } from "./VideoPlayer/VideoPlayer";
-export { default as Form } from "./Form/Form";
--- frontend_react/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./Home";
-export { default as About } from "./About";
-export { default as Services } from "./Services";
-export { default as Gallery } from "./Gallery";
-export { default as Contact } from "./Contact";
-export { default as Watch } from "./Watch";
-export { default as Error } from "./Error";
